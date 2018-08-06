07bda0ecd63a116d36a94da957a3ae527929f0fe
Adolfo <chuquiyauriadolfo@gmail.com>
2018-08-06 17:42:26 -0500

    Esta app esta siendo integrada a GitHub

---
+++ app/src/main/java/aplicacion/android/adolfo2/myapplication/Test.java
@@ -0,0 +1,12 @@
+package aplicacion.android.adolfo2.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+
+    public static void myToast(Context context){
+        Toast.makeText(context,"Hola amigos",Toast.LENGTH_LONG).show();
+    }
+}
